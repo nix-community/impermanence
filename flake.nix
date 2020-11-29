c7906f61a393a76d5e572e3b0e603423da1f0c5c
Cole Helbling <cole.e.helbling@outlook.com>
2020-11-28 19:01:20 -0800

    flake: remove unused nixpkgs input
    Packages are retrieved from the `pkgs` module argument, rather than
    Nixpkgs itself.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09-small";
-
-  outputs = { self, nixpkgs }: {
+  outputs = { self }: {
     nixosModules.impermanence = import ./nixos.nix;
   };
 }
