7a36ba9279fe8abccf5be2820bd60d4ec9d86207
crunchcat <33202689+crunchcat@users.noreply.github.com>
2022-02-10 22:39:01 -0500

    Add default NixOS module to flake

--- flake.nix
+++ flake.nix
@@ -2,5 +2,6 @@
   outputs = { self }: {
     nixosModules.impermanence = import ./nixos.nix;
     nixosModules.home-manager.impermanence = import ./home-manager.nix;
+    nixosModule = self.nixosModules.impermanence;
   };
 }
