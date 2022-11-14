d163deea46be36891a6ee22981173ae218ae1fc7
KFears <kfearsoff@gmail.com>
2022-11-15 02:31:08 +0400

    Revert "feat: symlink the file instead of erroring out"
    This reverts commit 8ea1ccacc879b2a6008d3535e1d59ae9cf6730e7.

--- mount-file.bash
+++ mount-file.bash
@@ -28,9 +28,8 @@ if [[ -L "$mountPoint" && $(readlink -f "$mountPoint") == "$targetFile" ]]; then
 elif mount | grep -F "$mountPoint"' ' >/dev/null && ! mount | grep -F "$mountPoint"/ >/dev/null; then
     echo "mount already exists at $mountPoint, ignoring"
 elif [[ -e "$mountPoint" ]]; then
-    echo "a file already exists at $mountPoint, turning it into symlink"
-    cp "$mountPoint" "$targetFile"
-    ln -s "$targetFile" "$mountPoint"
+    echo "A file already exists at $mountPoint!" >&2
+    exit 1
 elif [[ -e "$targetFile" ]]; then
     touch "$mountPoint"
     mount -o bind "$targetFile" "$mountPoint"
