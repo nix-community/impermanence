e30b36fb217660987a8a81074d204860aed21225
KFears <kfearsoff@gmail.com>
2022-11-15 02:30:33 +0400

    refactor: make essential presets toggle-able

--- presets/default.nix
+++ presets/default.nix
@@ -11,6 +11,13 @@ let
     directories = optionals conditional (value.directories or [ ]);
   };
 
+  buildEssentialPreset = name: value: let
+    conditional = (value.option or true) && config.presets.essential."${name}";
+  in {
+    files = optionals conditional (value.files or [ ]);
+    directories = optionals conditional (value.directories or [ ]);
+  };
+
   buildEntries = attrs: builtins.mapAttrs (name: value: mkOption {
     type = bool;
     default = true;
@@ -18,16 +25,13 @@ let
     description = "Whether to enable ${name} preset.";
   }) attrs;
 
-  essential = { 
-    option = config.presets.essential.enable;
-    files = [ "/etc/machine-id" ];
-    directories = [ "/var/lib/nixos" ];
-  };
+  systemPresets = builtins.mapAttrs (name: value: buildPreset name value)
+    (optionalAttrs config.presets.system.enable (import ./system.nix { inherit systemConfig lib; }));
 
-  allPresets = builtins.mapAttrs (name: value: buildPreset name value)
-  ({ inherit essential; } //
-  optionalAttrs config.presets.system.enable (import ./system.nix { inherit systemConfig lib; }));
+  essentialPresets = builtins.mapAttrs (name: value: buildEssentialPreset name value)
+    (optionalAttrs config.presets.essential.enable (import ./essential.nix { inherit systemConfig lib; }));
 
+  allPresets = essentialPresets // systemPresets;
   appliedPresets = foldAttrs (val: col: val ++ col) [] (builtins.attrValues allPresets);
 in
 {
@@ -35,11 +39,14 @@ in
   directories = appliedPresets.directories;
 
   presets = {
-    essential.enable = mkEnableOption ''
-      essential presets. Without those, you will likely get an unusable,
-      broken, or prone to corrupting over time system. It is recommended
-      to back up those entries
-    '';
+    essential = {
+      enable = mkEnableOption ''
+        essential presets. Without those, you will likely get an unusable,
+        broken, or prone to corrupting over time system. It is recommended
+        to back up those entries
+      '';
+    } // buildEntries (import ./essential.nix {inherit systemConfig lib; });
+
     system = {
       enable = mkEnableOption ''
         system presets. Those are not necessary for having a working system,
@@ -47,12 +54,6 @@ in
         for Network Manager goes in here. It is not recommended to
         back up those entries
       '';
-      essential = mkOption { # FIXME: we shouldn't need that
-        type = bool;
-        default = true;
-        internal = true;
-        description = "Whether to enable essential preset.";
-      };
     } // buildEntries (import ./system.nix {inherit systemConfig lib; });
   };
 }
