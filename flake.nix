89af9df5d0fb7ceb2fef8e0ca13477bf8b459b0f
mk3z <52108954+mk3z@users.noreply.github.com>
2023-01-14 18:19:39 +0200

    Add home manager module

--- flake.nix
+++ flake.nix
@@ -3,5 +3,6 @@
     nixosModules.impermanence = import ./nixos.nix;
     nixosModules.home-manager.impermanence = import ./home-manager.nix;
     nixosModule = self.nixosModules.impermanence;
+    hmModule = self.nixosModules.home-manager.impermanence;
   };
 }
