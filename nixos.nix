a0b6d84f54438c2219d28d6e6a9460bbccb36f00
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-02 17:23:19 +0100

    nixos: Recommend persisting /var/lib/nixos
    The `/var/lib/nixos` directory contains the uid and gid map for entities
    without a static id. Not persisting them means your user and group ids
    could change between reboots, which is likely undesirable.

--- nixos.nix
+++ nixos.nix
@@ -285,6 +285,7 @@ in
                     example = [
                       "/var/log"
                       "/var/lib/bluetooth"
+                      "/var/lib/nixos"
                       "/var/lib/systemd/coredump"
                       "/etc/NetworkManager/system-connections"
                     ];
@@ -349,6 +350,7 @@ in
             directories = [
               "/var/log"
               "/var/lib/bluetooth"
+              "/var/lib/nixos"
               "/var/lib/systemd/coredump"
               "/etc/NetworkManager/system-connections"
               { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
