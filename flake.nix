b8be97858b947a7438dbf679fce582f818046294
Matias Zwinger <matias.zwinger@protonmail.com>
2023-09-18 20:29:00 +0300

    Use plural attribute names and add default modules
    (cherry picked from commit e8c3d195069584b27196a5f6c37cd166c65797a9)

--- flake.nix
+++ flake.nix
@@ -1,8 +1,13 @@
 {
   outputs = { self }: {
     nixosModules.impermanence = import ./nixos.nix;
-    nixosModules.home-manager.impermanence = import ./home-manager.nix;
+    nixosModules.default = self.nixosModules.impermanence;
+
+    homeManagerModules.impermanence = import ./home-manager.nix;
+    homeManagerModules.default = self.homeManagerModules.impermanence;
+
+    # Deprecated
     nixosModule = self.nixosModules.impermanence;
-    hmModule = self.nixosModules.home-manager.impermanence;
+    nixosModules.home-manager.impermanence = self.homeManagerModules.impermanence;
   };
 }
