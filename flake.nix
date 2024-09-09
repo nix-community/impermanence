caa9775a43f70eda86c838641c00e2bba0af695b
Youssef <siryoussef@hotmail.com>
2024-09-09 21:53:06 +0300

    Correct homeManagerModule name for easy usage

--- flake.nix
+++ flake.nix
@@ -3,6 +3,6 @@
     nixosModules.impermanence = import ./nixos.nix;
     nixosModules.home-manager.impermanence = import ./home-manager.nix;
     nixosModule = self.nixosModules.impermanence;
-    homeManagerModules.impermanence = import ./home-manager.nix
+    homeManagerModules.impermanence = import ./home-manager.nix;
   };
 }
