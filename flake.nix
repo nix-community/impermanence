e09dd717bb2c8190f3f00f71d1f7030c97d455f4
siryoussef <53839210+siryoussef@users.noreply.github.com>
2024-09-09 21:37:20 +0300

    Update flake.nix
    homeManagerModules

--- flake.nix
+++ flake.nix
@@ -3,5 +3,6 @@
     nixosModules.impermanence = import ./nixos.nix;
     nixosModules.home-manager.impermanence = import ./home-manager.nix;
     nixosModule = self.nixosModules.impermanence;
+    homeManagerModules.impermanence = import ./home-manager.nix
   };
 }
